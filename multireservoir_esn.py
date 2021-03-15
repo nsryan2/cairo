2307259e7d7f18924838775fe16b26277172cc5b
Nathan Ryan <nsryan2@illinois.edu>
2021-03-14 22:19:34 -0500

    initialize the multires file

---
+++ multireservoir_esn.py
@@ -0,0 +1,23 @@
+import pyESN.pyESN as pyESN
+from tools import *
+from optimizers import *
+from pyESN.pyESN import ESN
+
+class MRESN(ESN):
+    """docstring for MRESN."""
+
+    def __init__(self, n_reservoirs):
+        super(MRESN, self).__init__(n_inputs = n_outputs)
+        self.n_reservoirs = n_reservoirs
+    
+    def _mupdate(self,)
+        
+    
+    def multi_prediction():
+
+if __name__ == "__main__":
+    test = MRESN(n_reservoirs=1)
+
+    print(test.n_reservoirs)
+    print(test.n_inputs)
+    print(test.n_outputs)
